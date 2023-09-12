bf59e62b73511c3e482fdb19154c7f19e247a809
Krish Srivastava <krish22092003@gmail.com>
2023-09-13 00:33:47 +0530

    wefew

--- Searching Algorithms/binarySearch(iterative).cpp
+++ Searching Algorithms/binarySearch(iterative).cpp
@@ -55,3 +55,9 @@ int main(void)
         : cout << "Element is present in the array at index" << result;
     return 0;
 }
+/*
+Time Complexity: 
+Best Case: O(1)
+Average Case: O(log N)
+Worst Case: O(log N)
+Auxiliary Space: O(1), If the recursive call stack is considered then the auxiliary space will be O(logN).*/
