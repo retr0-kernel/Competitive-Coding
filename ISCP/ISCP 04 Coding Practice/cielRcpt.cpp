42f175ce08055e143f0491d1edea10222365569e
Krish Srivastava <krish22092003@gmail.com>
2023-09-15 14:45:11 +0530

    wrvww

--- ISCP/ISCP 04 Coding Practice/cielRcpt.cpp
+++ ISCP/ISCP 04 Coding Practice/cielRcpt.cpp
@@ -1,2 +1,32 @@
 /*https://www.codechef.com/problems/CIELRCPT*/
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	vector<int>ar={1,2,4,8,16,32,64,128,256,512,1024,2048};
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int bill;
+	    cin>>bill;
+	    int j=ar.size()-1;
+	    int finalcount=0;
+	    while(bill!=0)
+	    {
+	        if(bill>=ar[j])
+	        {
+	            bill=bill-ar[j];
+	            finalcount++;
+	        }
+	        else
+	        {
+	            j--;
+	        }
+	    }
+	    cout<<finalcount<<endl;
+	}
+	return 0;
+}
